9280b949e05c7b47340378f3449e84a2e707667c
zlm051123 <3102512254@qq.com>
2025-12-25 12:00:35 +0800

    Delete 功能实现 directory

--- 功能实现/SMSplus/src/main/java/com/energy/smsplus/util/LocationDesensitizationUtil.java
+++
@@ -1,7 +0,0 @@
-package com.energy.smsplus.util;
-
-public class LocationDesensitizationUtil {
-    public static String desensitize(String location) {
-        return location.substring(0, 2) + "*" + location.substring(3);
-    }
-}
